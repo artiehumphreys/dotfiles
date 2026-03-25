e5dd0b701df9623416ca588cea2c66528bd7f693
artiehumphreys <artieh2003@gmail.com>
2026-03-25 12:58:50 -0400

    extracted sesh script

---
+++ tmux/sesh.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+export PATH=/opt/homebrew/bin:$PATH
+
+sesh connect "$(
+  sesh list | fzf-tmux -p 55%,60% \
+    --no-sort --reverse --ansi \
+    --border-label ' sesh ' \
+    --prompt '  ' \
+    --header '  ^a all ^t tmux ^g configs ^x zoxide ^d tmux kill ^f find' \
+    --bind 'tab:down,btab:up' \
+    --bind 'ctrl-a:change-prompt(  )+reload(sesh list --icons)' \
+    --bind 'ctrl-t:change-prompt(  )+reload(sesh list -t --icons)' \
+    --bind 'ctrl-g:change-prompt(  )+reload(sesh list -c --icons)' \
+    --bind 'ctrl-x:change-prompt(  )+reload(sesh list -z --icons)' \
+    --bind 'ctrl-f:change-prompt(  )+reload(fd -H -d 2 -t d -E .Trash . ~)' \
+    --bind 'ctrl-d:execute(tmux kill-session -t {E..})+change-prompt(  )+reload(sesh list --icons)'
+)"
