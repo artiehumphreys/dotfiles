c19c851533feb1a9b96e98dded582f2c20d5683a
artiehumphreys <artieh2003@gmail.com>
2026-07-09 15:46:53 -0400

    fix(init): set cppman buffer to nofile to prevent E141

--- nvim/init.lua
+++ nvim/init.lua
@@ -408,6 +408,9 @@ map("n", "<leader>fc", function()
 					local buf = vim.api.nvim_get_current_buf()
 					vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
 					vim.bo[buf].filetype = "man"
+					vim.bo[buf].buftype = "nofile"
+					vim.bo[buf].bufhidden = "wipe"
+					vim.bo[buf].swapfile = false
 					vim.bo[buf].modifiable = false
 				end)
 				return true
